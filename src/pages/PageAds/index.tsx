2824d23323087d2ffab27ab033791aaac5d18f1d
Luiz-de-Lima <luiz12359@gmail.com>
2023-03-13 21:27:15 -0300

    funcionalide de selecionar item feita

--- src/pages/PageAds/index.tsx
+++ src/pages/PageAds/index.tsx
@@ -2,7 +2,7 @@ import React, { ChangeEvent, useState, useEffect } from "react";
 import { PageArea } from "./styled";
 import { PageContainer } from "../../components/MainComponents";
 import { useApi } from "../../helpers/OlxifyApi";
-import { Link, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { typeState } from "../types/typesState";
 import { typeCategorie, typeAds } from "../types/typeCategory";
 import { AdItem } from "../../components/partials/Aditem";
@@ -10,7 +10,19 @@ import { AdItem } from "../../components/partials/Aditem";
 export const PageAds = () => {
   const [stateList, setStateList] = useState<typeState[]>([]);
   const [categories, setCategories] = useState<typeCategorie[]>([]);
+
   const [adsList, setAdsList] = useState<typeAds[]>([]);
+  const useQueryString = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  const query = useQueryString();
+
+  const [q, setQ] = useState(query.get("q") != null ? query.get("q") : "");
+  const [cat, setCat] = useState("");
+  const [stateSelect, setStateSelect] = useState(
+    query.get("cat") != null ? query.get("cat") : ""
+  );
 
   const getStates = async () => {
     const sList = await useApi.getStates();
@@ -38,7 +50,14 @@ export const PageAds = () => {
     <PageArea>
       <div className="left-side">
         <form action="" method="get">
-          <input type="text" name="q" id="" />
+          <input
+            type="text"
+            name="q"
+            id=""
+            placeholder="o que você procura?"
+            value={stateSelect}
+          />
+
           <div className="filterName">Estado:</div>
           <select name="state" id="">
             <option value=""></option>
@@ -52,7 +71,7 @@ export const PageAds = () => {
           <div className="filterName">Categoria:</div>
           <ul>
             {categories.map((item, index) => (
-              <li key={index} className="category-item">
+              <li key={index} className={cat===item.slug?'category-item active':'category-item'}>
                 {item.img}
                 <span>{item.name}</span>
               </li>
--- src/pages/PageAds/styled.ts
+++ src/pages/PageAds/styled.ts
@@ -7,6 +7,48 @@ export const PageArea = styled.div`
   .left-side {
     width: 250px;
     margin-right: 10px;
+
+    .filterName {
+      font-size: 15px;
+      margin: 10px 0;
+    }
+    input,
+    select {
+      width: 100%;
+      height: 40px;
+      border: solid 2px solid #9bb83c;
+      border-radius: 5px;
+      color: #000;
+      outline: none;
+      padding: 10px;
+    }
+    ul,
+    li {
+      margin: 0;
+      padding: 0;
+      list-style: none;
+    }
+    .category-item {
+      display: flex;
+      align-items: center;
+      padding: 10px;
+      border-radius: 5px;
+      cursor: pointer;
+
+      img {
+        width: 25px;
+        height: 25px;
+        margin-right: 5px;
+      }
+      span {
+        font-size: 14px;
+      }
+    }
+    .category-item:hover,
+    .category-item:active {
+      background-color: #9bb83c;
+      color: #fff;
+    }
   }
   .right-side {
     flex: 1;
